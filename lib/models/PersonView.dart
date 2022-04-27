2d07a6d30199d6e2a006f3c0cae2c8f66f113df7
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-28 01:29:37 +0200

    updated person view model.

--- lib/models/PersonView.dart
+++ lib/models/PersonView.dart
@@ -1,31 +1,59 @@
+import 'package:http/http.dart';
+
 class PersonView {
-  PersonView({
-    required this.url,
-    required this.name,
-    required this.gender,
-    required this.culture,
-    required this.born,
-    required this.died,
-    required this.father,
-    required this.mother,
-    required this.spouse,
-  });
+  final String _name;
+  final String _gender;
+  final String _culture;
+  final String _born;
+  final String _died;
+  final String _father;
+  final String _mother;
+  final String _spouse;
 
-  String url;
-  String name;
-  String gender;
-  String culture;
-  String born;
-  String died;
-  String father;
-  String mother;
-  String spouse;
+  PersonView(this._name, this._gender, this._culture, this._born, this._died,
+      this._father, this._mother, this._spouse);
 
-  bool hasDetail() {
-    return (father != "") && (spouse != "");
+  String get name {
+    if (_name == "") {
+      return "No Name";
+    } else {
+      return _name;
+    }
   }
 
-  bool notAlive() {
-    return (died != "");
+  String get gender {
+    return _gender;
+  }
+
+  String get culture {
+    return _culture;
+  }
+
+  String get born {
+    return _born;
+  }
+
+  String get died {
+    return _died;
+  }
+
+  String get father {
+    return _father;
+  }
+
+  String get mother {
+    return _mother;
+  }
+
+  String get spouse {
+    return _spouse;
+  }
+
+  bool hasDetail() {
+    return (_father != "") && (_spouse != "");
+  }
+
+  bool alive() {
+    return (_died == "");
   }
 }
