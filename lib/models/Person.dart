c45165bbc31cce7a599b8d96ffd622e70a429b7d
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-28 10:28:27 +0200

    remove unused datas from person model.

--- lib/models/Person.dart
+++ lib/models/Person.dart
@@ -11,11 +11,6 @@ class Person {
     required this.father,
     required this.mother,
     required this.spouse,
-    this.allegiances,
-    this.books,
-    this.povBooks,
-    this.tvSeries,
-    this.playedBy,
   });
 
   String url;
@@ -36,21 +31,15 @@ class Person {
   List<String>? playedBy;
 
   factory Person.fromJson(Map<String, dynamic> json) => Person(
-        url: json["url"],
-        name: json["name"],
-        gender: json["gender"],
-        culture: json["culture"],
-        born: json["born"],
-        died: json["died"],
-        titles: List<String>.from(json["titles"].map((x) => x)),
-        aliases: List<String>.from(json["aliases"].map((x) => x)),
-        father: json["father"],
-        mother: json["mother"],
-        spouse: json["spouse"],
-        allegiances: List<dynamic>.from(json["allegiances"].map((x) => x)),
-        books: List<String>.from(json["books"].map((x) => x)),
-        povBooks: List<dynamic>.from(json["povBooks"].map((x) => x)),
-        tvSeries: List<String>.from(json["tvSeries"].map((x) => x)),
-        playedBy: List<String>.from(json["playedBy"].map((x) => x)),
-      );
+      url: json["url"],
+      name: json["name"],
+      gender: json["gender"],
+      culture: json["culture"],
+      born: json["born"],
+      died: json["died"],
+      titles: List<String>.from(json["titles"].map((x) => x)),
+      aliases: List<String>.from(json["aliases"].map((x) => x)),
+      father: json["father"],
+      mother: json["mother"],
+      spouse: json["spouse"]);
 }
