15d70338d218def6590b8a982f94e87221152fb8
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-28 09:36:59 +0200

    cleaned bloc imports.

--- lib/bloc/home_bloc.dart
+++ lib/bloc/home_bloc.dart
@@ -1,9 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_code_challenge/models/PersonView.dart';
-import '../models/PersonsActivity.dart';
 import '../services/PersonsServices.dart';
-
 part 'home_event.dart';
 part 'home_state.dart';
 
