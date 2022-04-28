d17bf56993704ce6ab7db9fc6b273b7a8fc3788f
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-28 12:33:05 +0200

    refactored main file.

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_code_challenge/screens/home/Home.dart';
 import 'package:flutter_code_challenge/services/PersonsServices.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const PersonsApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class PersonsApp extends StatelessWidget {
+  const PersonsApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: MultiRepositoryProvider(
-          providers: [
-            RepositoryProvider(
-              create: (context) => PersonsService(),
-            )
-          ],
-          child: const HomePage(),
-        ));
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider(
+            create: (context) => PersonsService(),
+          )
+        ],
+        child: const HomePage(),
+      ),
+    );
   }
 }
