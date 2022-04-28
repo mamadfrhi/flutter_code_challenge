88891f1e9f802a76fddaf17519914d327786a684
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-28 14:36:03 +0200

    moved detai title container singleton class to separate file.

---
+++ lib/resources/TitlesContainer.dart
@@ -0,0 +1,8 @@
+class DetailsTitleContainer {
+  static DetailsTitleContainer shared = DetailsTitleContainer(); // singleton
+
+  String aliasesTitle = 'Aliases:\n';
+  String titlesTitle = 'Titles:\n';
+  String nameTitle = 'Name: ';
+  String cultureTitle = 'Culture: ';
+}
